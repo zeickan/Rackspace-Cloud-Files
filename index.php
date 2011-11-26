5d4ec36ccfa58741265eb27d72f576c427f4a088
Andros Romo <zeickan@gmail.com>
2011-11-25 23:20:49 -0600

    Demo

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once("cloud.class.php");
+
+
+
+$cloud = new cloud;
+
+$cloud->key = "AUTH-KEY";
+$cloud->usr = "USERNAME";
+
+$cloud->path = '/path/';
+
+if( $cloud->xAuth() ){
+
+if( $cloud->xSend("file.ext") ):
+
+    $cloud->path = '/images';
+
+    if( $cloud->publish() ){
+        
+        echo $cloud->publish_file;
+        
+    } else {
+     
+        echo $cloud->error;
+     
+    }
+    
+    
+
+else:
+
+    echo $cloud->error;
+
+endif;
+
+
+}
